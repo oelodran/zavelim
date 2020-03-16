67e59fba3501a2f6fcd1fcba83291d67c973ed29
Leonardo Ramljak <leonardo.ramljak@tel.net.ba>
2020-03-16 20:00:02 +0100

    nav, inex page

---
+++ public/index.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/custom.css">
+    <title>Zavelim</title>
+</head>
+
+<body>
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-light sticky-top" style="background-color: #e3f2fd;" >
+    <div class="container-fluid">
+
+        <a class="navbar-brand" href="index.php">Zavelim</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Naslovna</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">O nama</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Prodaja
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Oplate</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Viseće bine</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Tribine i podijumi</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Nosivi Tornjevi</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Usluge</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Akcija</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Kontakt</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!--- Image Slider -->
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="8"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="9"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="pictures/slides/oplata1.jpg" alt="First slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="pictures/slides/oplata2.jpg" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="pictures/slides/oplata3.jpg" alt="Third slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="pictures/slides/oplata4.jpg" alt="Third slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="pictures/slides/oplata5.jpg" alt="Third slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="pictures/slides/oplata6.jpg" alt="Third slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="pictures/slides/oplata7.jpg" alt="Third slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="pictures/slides/oplata8.jpg" alt="Third slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="pictures/slides/oplata9.jpg" alt="Third slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="pictures/slides/oplata10.jpg" alt="Third slide">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
+<!--- Jumbotron -->
+<div class="container-fluid">
+    <div class="row jumbotron">
+        <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-10">
+            <p class="lead">Beton je jedan od najčešće korištenih građevinskih materijala, zahvaljujući svojim izuzetnim svojstvima. Međutim, da biste stvorili građevinske elemente betonom, on se mora izliti u posebno dizajnirani kalup kojeg zovemo <b>oplata</b>.</p>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-2">
+            <a href="#"><button type="button" class="btn btn-outline-secondary btn-lg">Oplata</button></a>
+        </div>
+    </div>
+</div>
+
+<!--- Welcome Section -->
+<div class="container-fludi padding">
+    <div class="row welcome text-center">
+        <div class="col-12">
+            <h1 class="display-4">Gradite s lakoćom</h1>
+        </div>
+        <hr>
+        <div class="col-12">
+            <p class="lead">Mi posjedujemo dugogodišnje iskustvo u prodaji: oplata, visećih bina šperploča i ostale građevinske tehnike srodne s navedenim proizvodima.</p>
+        </div>
+    </div>
+</div>
+
+<!--- Three Column Section -->
+<div class="container-fluid padding">
+    <div class="row text-center padding">
+        <div class="col-xs-12 col-sm-6 col-md-4">
+            <i class="fas fa-code"></i>
+            <h3>OPLATA</h3>
+            <ul class="list-unstyled">
+                <li>Zidna oplata</li>
+                <li>Stropna oplata</li>
+                <li>Kanalna oplata</li>
+                <li>Zamjenske ploče i šperploče</li>
+                <li>Prodaja oplatnog pribora</li>
+            </ul>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4">
+            <i class="fas fa-bold"></i>
+            <h3>TEHNIKA</h3>
+            <p>Prodaja građevinske tehnike.</p>
+        </div>
+        <div class="col-sm-12 col-md-4">
+            <i class="fab fa-css3"></i>
+            <h3>VISEĆE BINE</V></h3>
+            <p>Viseće radne platforme na sajlama koriste se na velikim visinama ili za postavljanje montažnih fasada.</p>
+        </div>
+        <div class="col-sm-12 col-md-4">
+            <i class="fab fa-css3"></i>
+            <h3>TRIBINE I PODIJI</V></h3>
+            <p>Vrlo brza i jednostavna montaža. Praktično za razne sportska, zabavna i druga događanja. </p>
+        </div>
+        <div class="col-sm-12 col-md-4">
+            <i class="fab fa-css3"></i>
+            <h3>ZAŠTITNA OPREMA ZA RAD NA KROVOVIMA</V></h3>
+            <p>Jednostavnom montažom ostvarite sve uvjete za siguran rad na krovu.</p>
+        </div>
+        <div class="col-sm-12 col-md-4">
+            <i class="fab fa-css3"></i>
+            <h3>NOSIVI TORNJEVI</V></h3>
+            <p>Podupiranje oplate na velikim visinama s nosivim tornjevima ST 100 je sigurno i jednostavno.</p>
+        </div>
+    </div>
+</div>
+
+<!-- Footer -->
+<footer class="text-center page-footer font-small py-5" style="background-color: #e3f2fd;">
+    Zavelim  All Rights Reserved <?php echo date('Y'); ?>
+</footer>
+
+<script src="js/jquery.slim.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
---
+++ public/pictures/public_header.php
@@ -0,0 +1,99 @@
+<!doctype html>
+
+<html lang="hr">
+<head>
+    <title>S8S<?php if(isset($page_title)) { echo ' - ' . h($page_title);  echo ' preview'; }  ?><?php
+        if (isset($preview) && $preview) { echo ' [PREVIEW]'; } ?></title>
+    <meta name="google-site-verification" content="googlea03b63c2e56ac23f.html">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="keywords" content="skela, prodaja, građevinske, iznajmljivanje, oplate, pribor, prodaja, skele, industriska skela, varijabilna skela" />
+    <meta name="keywords" content="skela, prodaja, iznajmljivanje, oplate, pribor, prodaja, industriska skela" />
+    <meta name="description" content="Tvrtka S8S d.o.o. iz Posušja bavi se:
+    Najmom i prodajo fasadnih skela renomiranog proizvođača - Altrad Baumann
+    Prodajom oplate
+    Prodajom građevinske tehnike
+    Prodajom modularnih visećih bina
+    Prodajom montažnih tribina i podijuma
+    Prodajom zamjenske oplate" />
+    <meta name="author" content="Leonardo Ramljak" />
+    <meta name="Copyright" content="S8S" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/bootstrap.min.css'); ?>" />
+    <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/image_hover_effects.css'); ?>" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.css" />
+
+    <link rel="icon" type="image/vnd.mi" href="/images/logo.png" />
+</head>
+
+<body class="bg-dark">
+<div class="container p-0">
+    <header class="container text-center bg-secondary p-0">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="8"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="9"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="10"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="11"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="12"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="<?php echo url_for('/images/oplata1.jpg'); ?>" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="<?php echo url_for('/images/oplata3.jpg'); ?>" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="<?php echo url_for('/images/oplata5.jpg'); ?>" alt="Third slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="<?php echo url_for('/images/slajd/bina3.jpg'); ?>" alt="Fourth slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="<?php echo url_for('/images/slajd/bina4.jpg'); ?>" alt="Fifth slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="<?php echo url_for('/images/slajd/bina5.jpg'); ?>" alt="Sixth slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="<?php echo url_for('/images/slajd/bina7.jpg'); ?>" alt="Seventh slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="<?php echo url_for('/images/slajd/oplata7.jpg'); ?>" alt="Eighth slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="<?php echo url_for('/images/slajd/oplata8.jpg'); ?>" alt="Ninth slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="<?php echo url_for('/images/slajd/oplata10.jpg'); ?>" alt="Tenth slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="<?php echo url_for('/images/slajd/oplata4.jpg'); ?>" alt="Eleventh slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="<?php echo url_for('/images/slajd/tribina.jpg'); ?>" alt="Twelfth slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="<?php echo url_for('/images/slajd/tribine.JPG'); ?>" alt="Twelfth slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </header>
